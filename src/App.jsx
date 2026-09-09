1ed61f6db24c29ca0c5c72f9b6fea2487f6b8950
NoscarPlizk <kechakonno9131@gmail.com>
2026-09-09 23:01:59 +0800

    Colour Setting And Setting Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,35 @@ import { Container, Navbar, Nav } from 'react-bootstrap';
 import { TodoContext } from './todoContext';
 import useLocalStorage from 'use-local-storage';
 import Home from './pages/Home';
-import Addtodos from './pages/AddTodos';
+import Setting from './pages/Setting';
 import ErrorPage from './pages/ErrorPage';
 
-function Layout() {
+function Layout({ backgroundColor }) {
   return (
     <>
       <Navbar bg="light">
         <Container>
           <Navbar.Brand href="/">Home</Navbar.Brand>
           <Nav>
+            <Nav.Link href='/setting'>Setting</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Outlet />
+      <main 
+        style={{
+          backgroundColor: backgroundColor,
+          minHeight: 'calc(100vh - 56px)'
+        }}
+      >
+        <Outlet />
+      </main>
     </>
   );
 }
 
 export default function App() {
   const [ todo, setTodo ] = useLocalStorage('data', []);
+  const [ backgroundColor, setBackgroundColor ] = useLocalStorage('backgroundColor', '#f8f9fa');
 
   const AppNameThemeSet = {
     name: `Reading Tracker`,
@@ -31,11 +40,16 @@ export default function App() {
   };
   
   return (
-    <TodoContext.Provider value={{ todo, setTodo, AppNameThemeSet }}>
+    <TodoContext.Provider value={{ 
+      todo, setTodo, 
+      backgroundColor, setBackgroundColor,
+      AppNameThemeSet 
+    }}>
       <BrowserRouter >
         <Routes>
-          <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Layout backgroundColor={backgroundColor}/>}>
             <Route index element={<Home />} />
+            <Route path='setting' element={<Setting />} />
             <Route path="*" element={<ErrorPage />} />
           </Route>
         </Routes>
--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,6 +1,44 @@
+import { useContext } from 'react';
+import { TodoContext } from '../todoContext';
+import { Container, Form, Button } from 'react-bootstrap';
+
+
 export default function Setting() {
+    const {
+        backgroundColor,
+        setBackgroundColor
+    } = useContext(TodoContext);
+
+
     return (
         <div>
+            <Container>
+                <h1>Settings</h1>
+                <div className="mt-4">
+                    <label htmlFor="background-color">
+                        Background colour
+                    </label>
+                    <input
+                        id="background-color"
+                        type="color"
+                        value={backgroundColor}
+                        onChange={(event) =>
+                        setBackgroundColor(event.target.value)
+                        }
+                        style={{ width: '100px' }}
+                    />
+                </div>
+                <p className="mt-3">
+                    Selected colour: {backgroundColor}
+                </p>
+                <Button
+                    className="mt-2"
+                    variant="secondary"
+                    onClick={() => setBackgroundColor('#f8f9fa')}
+                >
+                    Reset colour
+                </Button>
+            </Container>
         </div>
     )
 }
